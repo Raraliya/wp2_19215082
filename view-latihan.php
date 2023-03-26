d0753e67c7cb0a9ae1789c65841816430299f2ed
raraliya <raraliya24@gmail.com>
2023-03-26 10:22:06 +0700

    menambah 4 file baru

---
+++ view-latihan.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+        <title>latihan1</title>
+</head>
+
+<body>
+    Hallo Kawan... Yuk kita belajar web programming...!! <br>
+    nilai 1 = <?= $nilai1; ?> <br>
+    nilai 2 = <?= $nilai2; ?> <br>
+    Ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+
+</body>
+</html>
